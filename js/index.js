898813d06115eab8e80a7fbe4cad73a19419c23a
Arongka Nafse Nawer <arongka.nafse.nawer@gmail.com>
2018-11-27 07:36:58 +0600

    Improved the look of the homepage by adding border

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
 // code to show current time and date on index page
 document.getElementById('date').innerHTML = new Date().toDateString();
+document.getElementById('time').innerHTML = new Date().toLocaleTimeString();
 
